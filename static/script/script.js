463a6b273aaacab4eb9cf9720e529cf7b8973082
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2024-01-03 17:32:41 +0100

    Projects carousel ongoing

--- static/script/script.js
+++ static/script/script.js
@@ -1,6 +1,13 @@
 const myCarouselElement = document.querySelector('#myCarousel')
 
+const projectsCarousel = document.querySelector('#projectsCarousel')
+
 const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 2000,
+    touch: false
+})
+
+const carousel2 = new bootstrap.Carousel(projectsCarousel, {
     interval: 2000,
     touch: false
 })
